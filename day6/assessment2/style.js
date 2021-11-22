b2fa00f3dde695a2f1529c27bdeaa862c45230ba
ramanath21 <94341757+ramanath21@users.noreply.github.com>
2021-11-22 10:02:44 +0530

    Add files via upload

---
+++ day6/assessment2/style.js
@@ -0,0 +1,19 @@
+function ChangeColor()  
+{  
+     document.getElementById('btn1').style.backgroundColor='Red';  
+    
+}  
+function ChangeColor2()  
+{  
+     document.getElementById('btn1').style.backgroundColor='Pink';  
+     
+}  
+function ChangeColor3()  
+{  
+     document.getElementById('btn1').style.backgroundColor='Green';  
+     
+}  
+function ChangeColor4()  
+{  
+     document.getElementById('btn1').style.backgroundColor='blue';  
+}  
